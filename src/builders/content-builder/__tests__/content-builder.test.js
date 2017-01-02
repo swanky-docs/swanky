9748caede96e65be843ad4a016b6bc5e8c3ab1ad
Rod Leviton <rod@rodleviton.com>
2017-01-02 18:03:43 +1100

    feat(app): add ability to proceess content from external source

--- src/builders/content-builder/__tests__/content-builder.test.js
+++ src/builders/content-builder/__tests__/content-builder.test.js
@@ -1,9 +1,22 @@
 'use strict';
 
+const path = require('path');
 const contentBuilder = require('./../');
 
 jest.mock('./../actions/read-content');
 jest.mock('./../actions/add-content');
+
+const mockPage = {
+  fileDependencies: [
+    {
+      type: 'content',
+      processedContent: [],
+      contentSrc: [
+        path.join(__dirname, './../__mocks__/__fixtures__/content', 'overview.md')
+      ]
+    }
+  ]
+};
 
 describe('contentBuilder', () => {
   it('should exist', () => {
@@ -11,7 +24,7 @@ describe('contentBuilder', () => {
   });
 
   it('should orchestrate build pipeline', (done) => {
-    contentBuilder.build().then((result) => {
+    contentBuilder.build(mockPage).then((result) => {
       expect(result).toEqual({});
       done();
     });
--- src/builders/content-builder/actions/read-content.js
+++ src/builders/content-builder/actions/read-content.js
@@ -2,23 +2,49 @@
 
 const Promise = require('bluebird');
 const fs = require('fs');
+const request = require('request');
+const readFile = Promise.promisify(fs.readFile);
 const DEFAULTS = require('./../../../constants.js');
+const isExternal = require('./../../../utils/is-external.js');
 
 /**
  * Read all content sources
- * @param {Object} page - the current page object to read content sources from
- * @returns {Object} - page content
+ * @param {Object} item - the current content object to read content sources from
+ * @returns {Object} - processed content
  */
-function readContent(page) {
-  return Promise.map(page.fileDependencies,
-    (item) => {
-      // Only process the remaining content sources
-      if (item.type === DEFAULTS.TYPE.CONTENT && !item.preprocessor) {
-        item.contentSrc.forEach((src, index) => {
-          item.processedContent[index] = fs.readFileSync(src, DEFAULTS.ENCODING.UTF8);
+function readContent(item) {
+  return Promise.map(item.contentSrc, (src) => {
+    // Only process the remaining content sources
+    if (item.type === DEFAULTS.TYPE.CONTENT && !item.preprocessor) {
+      if (isExternal(src)) {
+        return readExternalContent(src).then((content) => {
+          item.processedContent.push(content);
+        });
+      } else {
+        return readInternalContent(src).then((content) => {
+          item.processedContent.push(content);
         });
       }
-    }, { concurrency: 1 });
+    }
+  }).catch((err) => {
+    console.log('» swanky . ', err);
+  });
+}
+
+function readInternalContent(src) {
+  return readFile(src, DEFAULTS.ENCODING.UTF8);
+}
+
+function readExternalContent(src) {
+  return new Promise((resolve, reject) => {
+    request(src, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body);
+      }
+
+      reject('Error reading external url ( ' + src + ' ). Please check url and try again.');
+    });
+  });
 }
 
 module.exports = readContent;
