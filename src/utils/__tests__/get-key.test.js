68358c639520f635964b741be1da46d142e569ee
Rod Leviton <rod@rodleviton.com>
2017-01-13 19:18:21 +1100

    fix(build): replace \ with \ for page key

--- src/utils/__tests__/get-key.test.js
+++ src/utils/__tests__/get-key.test.js
@@ -15,6 +15,10 @@ describe('getKey', () => {
     expect(getKey('Bar', 'Foo')).toEqual('foo-bar');
   });
 
+  it('should replace the `&` character with `and`', () => {
+    expect(getKey('Setup & Installation')).toEqual('setup-and-installation');
+  });
+
   it('should strip html tags', () => {
     expect(getKey('<strong>Hello</strong>')).toEqual('hello');
   });
--- src/utils/get-key.js
+++ src/utils/get-key.js
@@ -3,11 +3,14 @@
 function getKey(pageTitle, parentTitle) {
   let str = parentTitle ? parentTitle + '-' + pageTitle : pageTitle;
 
-  return str.toLowerCase()
+  const result = str.toLowerCase()
     .replace(/(<([^>]+)>)/ig, '') // remove html tags
     .replace(/^[0-9.-_]+/g, '') // remove leading numbers
+    .replace(/&/g, 'and') // replace & with 'and'
     .replace(/[\s_-]+/g, '-') // swap any length of whitespace, underscore, hyphen characters with a single -
     .replace(/^-+|-+$/g, ''); // remove leading, trailing -
+
+  return result;
 }
 
 module.exports = getKey;
