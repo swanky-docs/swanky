967767f5b7ddb572c2be08e97fa0e4e8ec22405c
Rod Leviton <rod@rodleviton.com>
2017-01-29 14:18:52 +1100

    feat(build): update webpack to v2.2.0
    update deprecated fs-extra \ to use \ module instead
    
    minor release updates for
    
    other dependencies
    
    ISSUES CLOSED: #9

--- src/loaders/bootstrap-loader/bootstrap-loader.js
+++ src/loaders/bootstrap-loader/bootstrap-loader.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const fs = require('fs-extra');
+const walkSync = require('klaw-sync');
 const loaderUtils = require('loader-utils');
 
 module.exports = function() {
   this.cacheable();
 
   const query = loaderUtils.parseQuery(this.query);
-  const files = fs.walkSync(query.src);
+  const files = walkSync(query.src);
 
   const matches = files.filter((file) => {
-    return file.match(/index.js$/);
+    return file.path.match(/index.js$/);
   });
 
   const scripts = matches.map((match) => {
-    return `require('${match.replace(process.cwd() + '/', '')}');`;
+    return `require('${match.path.replace(process.cwd() + '/', '')}');`;
   });
 
   const modifiedSource = `'use strict'\nmodule.exports = (function() {
