00996db6b9ee3b4c71c20605375a40a4876d6320
Rod Leviton <rod@rodleviton.com>
2017-03-10 11:17:56 +1100

    fix(build): remove deprecation warning from transient deps
    ISSUES CLOSED: #13

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const build = require('./config/build/build');
 const serve = require('./config/serve/serve');
 const createConfig = require('./utils/create-config');
 
+// Temporary hack to remove
+// (node:5192) DeprecationWarning: loaderUtils.parseQuery() received a non-string value which can be problematic, see https://github.com/webpack/loader-utils/issues/56
+// parseQuery() will be replaced with getOptions() in the next major version of loader-utils.
+process.noDeprecation = true;
+
 module.exports = {
   devServer: (options) => {
     const swankyConfigFilePath = options && options.configPath ? options.configPath : '';
