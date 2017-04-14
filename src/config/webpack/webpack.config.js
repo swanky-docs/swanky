64c0b98c249fb9afa2069c10587e19e9450d6028
Rod Leviton <rod@rodleviton.com>
2017-04-14 21:45:11 +1000

    fix(build): remove react-preset dependency
    BREAKING CHANGE:
    react preset dependency now needs to be configured from documentation app

--- src/config/webpack/webpack.config.js
+++ src/config/webpack/webpack.config.js
@@ -116,7 +116,7 @@ module.exports = (CONFIG, SWANKY_CONFIG) => {
           use: [{
             loader: 'babel-loader',
             query: {
-              presets: ['es2015', 'react']
+              presets: ['babel-preset-es2015'].map(require.resolve)
             }
           }]
         },
