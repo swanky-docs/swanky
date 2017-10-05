c9eb42e02b29050a0467a63358fb97bbea3e2bad
Thorsten Pohl <thorsten.pohl@lht.dlh.de>
2017-10-05 13:54:42 +0200

    feat - add support for an unlimited depth of subsections.

--- src/utils/get-sections-config.js
+++ src/utils/get-sections-config.js
@@ -3,6 +3,16 @@
 const pageFactory = require('./../factories/page-factory');
 const DEFAULTS = require('./../constants.js');
 
+const recursivelyAddSubSections = (section, item, siteMeta, root) => {
+  if (item.subSections) {
+    item.subSections.forEach((subsection)=>{
+      const page = pageFactory(subsection, siteMeta, root.title);
+      section.push(page);
+      recursivelyAddSubSections(section, subsection, siteMeta, root);
+    });
+  }
+};
+
 module.exports = (sections, siteMeta) => {
   const _sections = sections && sections.length ? sections : DEFAULTS.DEFAULT_SECTION;
 
@@ -10,15 +20,7 @@ module.exports = (sections, siteMeta) => {
     const page = pageFactory(item, siteMeta, null);
     let section = [].concat(page);
 
-    if (item.subSections) {
-
-      // Flatten sub-section into an array
-      section = item.subSections.reduce((acc, value) => {
-        const page = pageFactory(value, siteMeta, item.title);
-
-        return acc.concat(page);
-      }, section);
-    }
+    recursivelyAddSubSections(section, item,  siteMeta, page);
 
     return section;
 
